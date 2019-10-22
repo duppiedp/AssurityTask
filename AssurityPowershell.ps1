fc36cbc29eb9837bb4c150c7bef04daafc362440
duppiedp <duppiedp@gmail.com>
2019-10-22 10:28:13 +0200

    Check in 001
    - Clarify Comments
    - Change Test 3 to do a direct read of Gallery Promotion instead of iterating through all of them

--- AssurityPowershell.ps1
+++ AssurityPowershell.ps1
@@ -5,6 +5,12 @@
 # 3. The Promotions element with Name = "Gallery" has a Description that contains the text "2x larger image"
 ####
 
+#### Overall Method
+# These tests will be performed in 3 different stages, for the 3 different steps.
+# Each step will run and the result will be added to a Results array.
+# A summary will be returned where the results will be returned as well as a pass rate.
+####
+
 $apiEndpoint = "https://api.tmsandbox.co.nz/v1/"
 $apiMethod = "Categories/6327/Details.json?catalogue=false"
 $apiCallUrl = $apiEndpoint + $apiMethod
@@ -13,6 +19,8 @@ $testCaseResult = New-Object System.Collections.ArrayList
 $testCaseResult.Clear()
 
 function AddResult([string]$Test, [string]$Result, [string]$Comments = ""){
+    <# This is a function that appends to my result list
+    so I can output in the summary section #>
     $testResultObj = @{Test=$Test;Result=$Result;Comments=$Comments}
     $testCaseResult.Add($testResultObj) | Out-Null
 }
@@ -28,7 +36,7 @@ clear-host
 $apiOutput = Invoke-RestMethod -Method Get -Uri $apiCallUrl
 
 #region Test 1
-# Test Case 1.
+# Test Case 1. Name = "Carbon credits"
 write-host "Test Case 1:"
 $TestCase = "Carbon credits"
 $TestPass = $false
@@ -58,7 +66,7 @@ if ($TestPass){
 #endregion
 
 #region Test 2
-# Test Case 2.
+# Test Case 2. CanRelist = TRUE (NOTE: Not True or true)
 write-host "Test Case 2:"
 $TestCase = "TRUE"
 $TestPass = $false
@@ -94,19 +102,14 @@ $TestPass = $false
 $TestNr = 3
 $TestFailComments = "Value could not be found."
 try{
-    foreach($PromoItem in $apiOutput.Promotions){
-        if ($PromoItem.Name -eq "Gallery"){
-            #we can now start the test
-            if ($PromoItem.Description -cmatch $TestCase){
-                write-host "Pass" -f Green
-                $TestPass = $true
-                break;
-            } else {
-                write-host "Fail" -f Red
-                $TestFailComments = "   Expected : $TestCase `n   Actual : $($PromoItem.Description)"
-                break;
-            }
-        }
+    #first, find the Gallery Promotion
+    $PromoItem = $apiOutput.Promotions | where { $_.Name -eq "Gallery" }
+    if ($PromoItem.Description -cmatch $TestCase){
+        write-host "Pass" -f Green
+        $TestPass = $true
+    } else {
+        write-host "Fail" -f Red
+        $TestFailComments = "   Expected : $TestCase `n   Actual : $($PromoItem.Description)"
     }
 }
 catch {
